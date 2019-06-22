6972e2039891cc8719c497c97ec9cafe7893c057
Lo-que <Lo-que@foxmail.com>
2019-06-22 23:16:47 +0800

    0112-Path-Sum init

---
+++ solutions/0112-Path-Sum/0112-Path-Sum.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, sum) {
+  if (!root) return false
+  sum -= root.val
+  if (!root.left && !root.right) return (sum === 0)
+  return hasPathSum(root.left, sum) || hasPathSum(root.right, sum)
+}
