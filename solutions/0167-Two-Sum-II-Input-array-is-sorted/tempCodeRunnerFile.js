e6a110acd1d446a6209f343b2f1d672567dc06ee
Lo-que <Lo-que@foxmail.com>
2019-07-03 23:08:16 +0800

    delete tempCodeRunnerFile.js

--- solutions/0167-Two-Sum-II-Input-array-is-sorted/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-var twoSum = function(numbers, target) {
-  let map = {}
-  for (let i = 0, len = numbers.length; i < len; i++) {
-    if (map[numbers[i]] !== undefined) return [map[numbers[i]] + 1, i + 1]
-    map[target - numbers[i]] = i
-  }
-}
