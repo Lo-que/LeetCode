0c7abc7aca3aa066a86820b024982b71a5132e09
Lo-que <Lo-que@foxmail.com>
2019-07-05 22:12:55 +0800

    modified 0169-Majority-Element/0169-Majority-Element.js

--- solutions/0169-Majority-Element/0169-Majority-Element.js
+++ solutions/0169-Majority-Element/0169-Majority-Element.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-
+  let res = nums[0]
+  let count = 0
+  for(num of nums) {
+    if(count === 0) res = num
+    count += res === num ? 1 : -1
+  }
+  return res
 }
