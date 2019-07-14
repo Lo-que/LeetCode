a9197712eaa135e927eb6bbc878b98bc57acf13c
Lo-que <Lo-que@foxmail.com>
2019-07-14 21:22:49 +0800

    0190-Reverse-Bits init

---
+++ solutions/0190-Reverse-Bits/0190-Reverse-Bits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+  n = n >>> 0
+  let i = 32
+  let res = 0
+  while (i--) {
+    res <<= 1
+    res += n & 1
+    n >>>= 1
+  }
+  return res
+}
+console.log(reverseBits(11111111111111111111111111111101))
