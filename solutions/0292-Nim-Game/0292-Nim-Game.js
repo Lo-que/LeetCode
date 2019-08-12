08058662099173656c5263aa651aa875633e7e51
Lo-que <Lo-que@foxmail.com>
2019-08-12 22:53:24 +0800

    0292-Nim-Game init

---
+++ solutions/0292-Nim-Game/0292-Nim-Game.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: qudongqi
+ * @Date: 2019-08-12 22:35:01
+ * @LastEditors: qudongqi
+ * @LastEditTime: 2019-08-12 22:53:04
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+  return n % 4 !== 0
+}
