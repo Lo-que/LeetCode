171ec8bb58688877ae89e5cfff0832620c159485
Lo-que <Lo-que@foxmail.com>
2019-08-14 23:06:54 +0800

    0326-Power-of-Three init

---
+++ solutions/0326-Power-of-Three/0326-Power-of-Three.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: qudongqi
+ * @Date: 2019-08-14 22:57:05
+ * @LastEditors: qudongqi
+ * @LastEditTime: 2019-08-14 23:04:59
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+  return n > 0 && 1162261467 % n == 0
+}
