0658735850cae9c7a93d8228926ec455b3cc099e
Lo-que <Lo-que@foxmail.com>
2019-10-07 22:40:52 +0800

    0557-Reverse-Words-in-a-String-III init

---
+++ solutions/0557-Reverse-Words-in-a-String-III/0557-Reverse-Words-in-a-String-III.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  return s
+    .split(' ')
+    .map(item => {
+      return item
+        .split('')
+        .reverse()
+        .join('')
+    })
+    .join(' ')
+}
